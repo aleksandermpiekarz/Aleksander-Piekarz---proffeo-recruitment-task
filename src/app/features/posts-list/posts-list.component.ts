b80574850e39ed437d67d3bc4c70ffa37a656342
Aleksander Piekarz <aleksander.m.piekarz@gmail.com>
2025-09-19 14:42:22 +0200

    basic styling done for posts-list with mocked be

--- src/app/features/posts-list/posts-list.component.ts
+++ src/app/features/posts-list/posts-list.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-posts-list',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './posts-list.component.html',
 })
 export class PostsListComponent {
+  // Temporary mock
+  public posts = signal<any>([
+    {
+      userId: 1,
+      id: 1,
+      title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: 'qui est esse',
+      body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
+    },
+  ]);
 
+  public constructor(private router: Router) {}
+
+  public apply(): void {}
+
+  public openDetails(id: number): void {
+    this.router.navigate(['/posts', id]);
+  }
+
+  public toggleFavorite(id: number): void {}
+
+  public filterOnlyFavorites(shouldFilter: boolean): void {}
+
+  public filterByUserId(userId: number): void {}
+
+  public filterByText(searchText: string): void {}
 }
